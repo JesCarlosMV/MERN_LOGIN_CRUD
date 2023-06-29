1ebccaf14344ecf743a273282d7e1d5ba30a93a2
JesCarlosMV <amgtk@hotmail.com>
2023-06-29 20:18:26 +0200

    react router dom starting

--- Front/src/main.jsx
+++ Front/src/main.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/login" element={<h1>login</h1>} />
+        <Route path="/register" element={<h1>register</h1>} />
+        <Route path="/tasks" element={<h1>tasks</h1>} />
+        <Route path="/add-tasks" element={<h1>add tasks</h1>} />
+        <Route path="/tasks/:id" element={<h1>tasks with id</h1>} />
+        <Route path="/profile" element={<h1>profile</h1>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>
+);
