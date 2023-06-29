804b03dc3fe15edc59e48ab63bc21c6ff4075070
JesCarlosMV <amgtk@hotmail.com>
2023-06-29 10:19:13 +0200

    task model added

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,8 +1,23 @@
+import Task from '../models/task.model.js';
+
 export const getTasks = (req, res) => {};
 
 export const getTask = (req, res) => {};
 
-export const addTask = (req, res) => {};
+export const addTask = (req, res) => {
+  console.log(req.body);
+  console.log(req.user.id);
+
+  const { title, description } = req.body;
+
+  const newTask = new Task({
+    title,
+    description,
+    user: req.user.id,
+  });
+
+  newTask.save();
+};
 
 export const deleteTask = (req, res) => {};
 
---
+++ src/models/task.model.js
@@ -0,0 +1,24 @@
+import mongoose from 'mongoose';
+
+const TaskSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    description: {
+      type: String,
+      trim: true,
+    },
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model('Task', TaskSchema);
