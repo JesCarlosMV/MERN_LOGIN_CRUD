0d09c6ec371d3e7ac99aa43de57a362881a2c8f1
JesCarlosMV <amgtk@hotmail.com>
2023-06-29 16:52:49 +0200

    task controller done 5/5

--- src/controllers/tasks.controller.js
+++ src/controllers/tasks.controller.js
@@ -1,9 +1,32 @@
-// IMPORTANTE => ASYNC AWAIT PARA TRABAJAR CON LAS PROMESAS DE MONGOOSE EN LA BD
+/* IMPORTANTE => ASYNC AWAIT PARA TRABAJAR CON LAS PROMESAS DE MONGOOSE EN LA BD
+
+- getTasks: Obtiene todas las tareas de DE UN USUARIO 
+                             ( id del usuario en el token => req.user.id )
+- getTask: Obtiene una tarea por id      ( id de la tarea => req.params )
+- createTask: Crea una tarea         ( title, description => req.body )
+- deleteTask: Elimina una tarea por id   ( id de la tarea => req.params )
+- updateTaks: Actualiza una tarea por id ( id de la tarea => req.params 
+                                       title, description => req.body )
+
+
+ */
 import Task from '../models/task.model.js';
 
-export const getTasks = (req, res) => {};
+export const getTasks = async (req, res) => {
+  const { id } = req.user;
+  const listaTareasEntera = await Task.find({
+    user: id,
+  });
 
-export const getTask = async (req, res) => {
+  if (listaTareasEntera.length == 0)
+    return res.status(404).json({
+      message: 'No hay tareas para mostrar :(',
+    });
+
+  return res.status(200).json(listaTareasEntera);
+};
+
+export const get_1_Task = async (req, res) => {
   const { id } = req.params;
 
   const taskFound = await Task.findById(id);
@@ -16,7 +39,7 @@ export const getTask = async (req, res) => {
 
 export const createTask = (req, res) => {
   const { title, description } = req.body;
-  const { id } = req.params;
+  const { id } = req.user;
 
   if (!title || !description)
     return res
@@ -50,4 +73,29 @@ export const deleteTask = async (req, res) => {
   return res.status(200).json({ message: 'Tarea Borrada! :D ' });
 };
 
-export const updateTaks = (req, res) => {};
+export const updateTaks = async (req, res) => {
+  const { id } = req.params;
+  const { title, description } = req.body;
+
+  if (!title || !description)
+    return res
+      .status(400)
+      .json({ message: 'Te falta o titulo o descripcion de la tarea :(' });
+
+  const taskUpdated = await Task.findByIdAndUpdate(
+    id,
+
+    {
+      title,
+      description,
+    },
+    { new: true }
+  );
+
+  if (!taskUpdated)
+    return res
+      .status(404)
+      .json({ message: 'Tarea no encontrada para actualizar :(' });
+
+  return res.status(200).json({ message: 'Tarea Actualizada! :D ' });
+};
--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,7 +3,7 @@ import { validateToken } from '../middlewares/validateToken.js';
 import {
   createTask,
   deleteTask,
-  getTask,
+  get_1_Task,
   getTasks,
   updateTaks,
 } from '../controllers/tasks.controller.js';
@@ -12,7 +12,7 @@ const router = Router(); // router de las rutas de las tareas, para usarlo en el
 
 router.post('/tasks', validateToken, createTask); // insertar tarea
 router.get('/tasks', validateToken, getTasks); // ver todas las tareas
-router.get('/tasks/:id', validateToken, getTask); //ver solo una tarea por id
+router.get('/tasks/:id', validateToken, get_1_Task); //ver solo una tarea por id
 router.delete('/tasks/:id', validateToken, deleteTask); // eliminar tarea por id
 router.put('/tasks/:id', validateToken, updateTaks); // modificar una tarea por id
 
